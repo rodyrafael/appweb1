36e3a00be42483093b61eda66c49e056436491e0
rody2024 <elrafa921@hotmail.com>
2025-04-24 11:30:23 -0500

    guardado de clientes

--- Pages/Clientes/Create.cshtml.cs
+++ Pages/Clientes/Create.cshtml.cs
@@ -1,3 +1,5 @@
+using appweb1.Data;
+using appweb1.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,19 @@ namespace MyApp.Namespace
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly AppDbContext _ctx;
+        public CreateModel(AppDbContext ctx){
+            _ctx=ctx;
+        }
+
+        [BindProperty]
+        public Cliente Cliente {get;set;}
+        public void OnGet(){}
+
+        public IActionResult OnPost(){
+            _ctx.Clientes.Add(Cliente);
+            _ctx.SaveChanges();
+            return RedirectToPage("Index");
         }
     }
 }
